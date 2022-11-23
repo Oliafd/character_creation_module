55441b007fee6ba012f06d4a55a7c51cca594ab4
Ольга Фёдорова <fedor77@mail.ru>
2022-11-23 10:59:04 +0300

    with docstring

---
+++ docsrt.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+message: str = ('Добро пожаловать в самую лучшую программу для вычисления'
+                ' квадратного корня из заданного числа')
+
+
+def calculate_square_root(number) -> float:
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number: float) -> None:
+    """Вычисляет корень заданного числа."""
+    if your_number <= 0:
+        return
+    root = calculate_square_root(your_number)
+    print(f'Мы вычислили квадратный корень из '
+          f'введённого вами числа.'
+          f' Это будет: {root}')
+
+
+print(message)
+calc(25.5)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Генерирует количество очков, зависит от персонажа."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
                 f'{5 + randint(3, 5)}')
@@ -14,6 +15,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Генерирует количество очков защиты, зависит от персонажа."""
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
@@ -23,6 +25,9 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Возвращает сообщение о применении спец умения,.
+    зависит от выбранного персонажа.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 f'«Выносливость {80 + 25}»')
@@ -33,6 +38,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Тренировка с выбранным персонажем."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +63,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбор персонажа."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -79,6 +86,7 @@ def choice_char_class() -> str:
 
 
 def main() -> str:
+    """Запускает игру и вызывает вспомогательные функции."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -90,3 +98,4 @@ def main() -> str:
 
 
 main()
+
